304281fb9087c1594224e80c64e091ca64ee7f74
Ikadev95 <fegughi@gmail.com>
2024-10-08 15:36:31 +0200

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp1Component } from './Components/comp1/comp1.component';
+import { Comp2Component } from './Components/comp2/comp2.component';
+import { Comp3Component } from './Components/comp3/comp3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component,
+    Comp2Component,
+    Comp3Component
   ],
   imports: [
     BrowserModule,
